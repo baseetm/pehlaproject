1c8a30cb6d1583278ccff6fc5fa13d913dea130e
sudh1994 <46997211+sudh1994@users.noreply.github.com>
2024-07-25 21:27:51 +0530

    Update Environment.sh

--- Environment.sh
+++ Environment.sh
@@ -7,17 +7,9 @@ cd myworkspace2
 apt install python3-pip
 pip install Flask mysql-connector-python
 python3 app.py
-sudo systemctl status mysql
-sudo mysql -u root -p
 sudo apt update
 sudo apt install mysql-server -y
 sudo mysql_secure_installation
-sudo mysql -u root -p
-CREATE DATABASE webapp_db;
-CREATE USER 'webapp_user'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON webapp_db.* TO 'webapp_user'@'%';
-FLUSH PRIVILEGES;
-EXIT;
 apt install net-tools
 sudo ufw allow 3306/tcp
 #########chmod +x mysql1.sh
