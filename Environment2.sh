75593ddca4b2a8158c65033f8142d68f04769fe9
AjayPrakash25 <110064264+AjayPrakash25@users.noreply.github.com>
2024-07-26 01:57:01 +0530

    Update Environment2.sh

--- Environment2.sh
+++ Environment2.sh
@@ -7,4 +7,7 @@ sudo systemctl enable apache2
 sudo apt install php libapache2-mod-php php-mysql
 sudo apt install mysql-server
 sudo mysql_secure_installation
-
+git config --user.name "sudh1994"
+git config --global user.name "sudh1994"
+git config --global user.email "sudhanshu7492@gmail.com"
+git clone https://github.com/baseetm/pehlaproject.git
