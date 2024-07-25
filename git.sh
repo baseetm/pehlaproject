f37529a7d923ce8c39aa928eb791d8076dc6a275
sudh1994 <46997211+sudh1994@users.noreply.github.com>
2024-07-25 22:18:26 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,3 +1,5 @@
+sudo apt update
+sudo apt install git -y
 git config --global user.name "sudh1994"
 git config --global user.email "sudhanshu7492@gmail.com"
 git clone https://github.com/baseetm/pehlaproject.git
