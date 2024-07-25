8a208f705617973e8b6f37548251da425c720a6f
sudh1994 <46997211+sudh1994@users.noreply.github.com>
2024-07-26 02:00:38 +0530

    Update Environment2.sh

--- Environment2.sh
+++ Environment2.sh
@@ -11,3 +11,5 @@ git config --user.name "sudh1994"
 git config --global user.name "sudh1994"
 git config --global user.email "sudhanshu7492@gmail.com"
 git clone https://github.com/baseetm/pehlaproject.git
+cd pehlaproject
+
